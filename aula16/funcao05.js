45310b10064693c688f7d8e51be39381dd35572c
MarVinCosMo <74730632+marvincosmo@users.noreply.github.com>
2021-04-20 08:50:23 -0300

    Término do curso de JavaScript

---
+++ aula16/funcao05.js
@@ -0,0 +1,9 @@
+// RECURSIVIDADE (quando a função faz a chamada para ela mesma)
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
